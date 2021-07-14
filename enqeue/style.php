bc10af04a4513667a38fb167848a97b1fb7c5bb5
Tsan Aung <myanmar@post.com>
2021-07-15 06:06:03 +0630

    Create style.php

---
+++ enqeue/style.php
@@ -0,0 +1,12 @@
+/**
+* functions.php
+**/
+
+wp_register_style( 'tsan-style', get_template_directory_uri() . '/sass/tsan.css', array(), rand(111,9999), 'all' );
+
+//Or
+
+function tsan_style() {
+	wp_enqueue_style( 'tsan-style', get_template_directory_uri() . '/css/tsan.css', array(), rand(111,9999), 'all' );
+	}
+	add_action('wp_enqueue_scripts', 'tsan_style');
