c663146a7d23b02f38648cf33a3ed54d22db4151
Tsan Aung <myanmar@post.com>
2021-07-15 06:07:14 +0630

    Update style.php

--- enqeue/style.php
+++ enqeue/style.php
@@ -2,10 +2,10 @@
 * functions.php
 **/
 
-wp_register_style( 'tsan-style', get_template_directory_uri() . '/sass/tsan.css', array(), rand(111,9999), 'all' );
+//Register
+wp_register_style( 'tsan-style', get_template_directory_uri() . '/css/tsan.css', array(), rand(111,9999), 'all' );
 
-//Or
-
+//Or Add action
 function tsan_style() {
 	wp_enqueue_style( 'tsan-style', get_template_directory_uri() . '/css/tsan.css', array(), rand(111,9999), 'all' );
 	}
