25ebd09fadf5b624b67de392c4c1bdf9299d775a
Tsan Aung <myanmar@post.com>
2021-07-30 11:54:46 +0630

    FA icons added

--- cwt/functions.php
+++ cwt/functions.php
@@ -8,6 +8,8 @@ function lmm_scripts() {
 
 	wp_enqueue_style( 'lovelymyanmar-bulma', get_template_directory_uri() . '/lmm/bulma.css', array(), 'all' ); //Bulma.CSS
 
+	wp_enqueue_style( 'lovelymyanmar-fa', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css', array(), 'all' ); //FA icons
+
 	wp_enqueue_script( 'lmm-js', get_template_directory_uri() . '/lmm/lmm.js', array(), _S_VERSION, true ); //lmm.js
 
 }
