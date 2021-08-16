ad15647aace6ee0b3e535bd55943db7ef0e46133
Tsan Aung <myanmar@post.com>
2021-08-17 05:01:29 +0630

    Create footer.php

---
+++ springmm_v2/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Spring_MM
+ */
+
+?>
+</section>
+		</div> <!-- root layout -->
+	</div> <!-- main -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
